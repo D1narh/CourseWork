858168f2bc758c4ee04f0e1275ce4252d4f6f60c
Гурьянов Денис <dunis.guryanov@mail.ru>
2023-01-28 02:52:47 +0400

    When selected in the products column, the product remains highlighted

---
+++ MaimApp/Views/MainWindow/MainProduct.xaml.cs
@@ -0,0 +1,262 @@
+﻿using MaimApp.Class;
+using MaimApp.Class.MainProductC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using MaimApp.BLL;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Media.Imaging;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+
+namespace MaimApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainProduct.xaml
+    /// </summary>
+    public partial class MainProduct : Window
+    {
+        private readonly Formatter _formatter = new Formatter();
+
+        int Press = 0;
+        public MainProduct()
+        {
+            InitializeComponent();
+
+            Sortierung();
+            ChangeSitys();
+            AirTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            BusTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            Hotels.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            Adventures.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+        }
+
+        public static string GetUserCountryByIp()
+        {
+            IpInfo ipInfo = new IpInfo();
+            try
+            {
+                string info = new WebClient().DownloadString("http://ipinfo.io");
+                ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+            }
+            catch (Exception)
+            {
+                ipInfo.Country = null;
+            }
+
+            return ipInfo.Country;
+        }
+
+        private void AirTickets_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(AirTickets);
+        }
+
+        private void AirTickets_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(AirTickets);
+        }
+
+        private void BusTickets_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(BusTickets);
+        }
+
+        private void BusTickets_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(BusTickets);
+        }
+
+        private void Adventures_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(Adventures);
+        }
+
+        private void Adventures_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(Adventures);
+        }
+
+        private void Hotels_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(Hotels);
+        }
+
+        private void Hotels_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(Hotels);
+        }
+        public void AnimationEnter(Button name)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.To = 140;
+            anim.Duration = TimeSpan.FromSeconds(0.1);
+            name.BeginAnimation(WidthProperty, anim);
+        }
+        public void AnimationLeave(Button name)
+        {
+            if (ButtonPressNow.Content.ToString() == name.Name.ToString())
+            {
+                return;
+            }
+            else
+            {
+
+                DoubleAnimation anim = new DoubleAnimation();
+                anim.To = 130;
+                anim.Duration = TimeSpan.FromSeconds(0.1);
+                name.BeginAnimation(WidthProperty, anim);
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadProduct();
+
+            animation.Visibility = Visibility.Hidden;
+            SearchText.Visibility = Visibility.Hidden;
+
+            GC.Collect();
+        }
+        public async Task LoadProduct()
+        {
+            ViewProduct viewProduct = new ViewProduct();
+
+            list.ItemsSource = await Task.Run(() => viewProduct.FillCatalog());
+        }
+
+        private void Sort_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            if (ChangeSortGrid.Visibility == Visibility.Hidden)
+            {
+                ChangeSortGrid.Visibility = Visibility.Visible;
+                anim.To = 145;
+                anim.Duration = TimeSpan.FromSeconds(0.25);
+                ChangeSortGrid.BeginAnimation(HeightProperty, anim);
+            }
+            else
+            {
+                anim.To = 0;
+                anim.Duration = TimeSpan.FromSeconds(0.1);
+                ChangeSortGrid.BeginAnimation(HeightProperty, anim);
+                ChangeSortGrid.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void Sortierung()
+        {
+            var i = new List<string> { "По скидке", "Лучшие отзывы", "Цена: Сначала дешевле", "Цена: Сначала дороже" };
+
+            foreach (var item in i)
+            {
+                Button button = new Button
+                {
+                    Content = item,
+                    FontSize = 18,
+                    FontFamily = new FontFamily("Merienda One"),
+                    Padding = new Thickness(10),
+                    Style = (Style)FindResource("ComboBoxButton")
+                };
+                ChangeSort.Children.Add(button);
+            }
+        }
+
+        private void City_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            if (ChangeCityGrid.Visibility == Visibility.Hidden)
+            {
+                ChangeCityGrid.Visibility = Visibility.Visible;
+                anim.To = 245;
+                anim.Duration = TimeSpan.FromSeconds(0.25);
+                ChangeCityGrid.BeginAnimation(HeightProperty, anim);
+            }
+            else
+            {
+                anim.To = 0;
+                anim.Duration = TimeSpan.FromSeconds(0.1);
+                ChangeCityGrid.BeginAnimation(HeightProperty, anim);
+                ChangeCityGrid.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void ChangeSitys()
+        {
+            var i = new List<string> { "Абакан", "Азов", "Александров", "Алексин", "Альметьевск", "Анапа", "Ангарск", "Анжеро-Судженск", "Апатиты", "Арзамас", "Армавир", "Арсеньев", "Артем", "Архангельск", "Асбест", "Астрахань", "Ачинск", "Балаково", "Балахна", "Балашиха", "Балашов", "Барнаул", " Батайск", "Белгород", "Белебей", "Белово", "Белогорск (Амурская область)", "Белорецк,Белореченск", "Бердск", "Березники", "Березовский (Свердловская область)", "Бийск", "Биробиджан", "Благовещенск (Амурская область)", "Бор", "Борисоглебск", "Боровичи", "Братск", "Брянск", "Бугульма", "Буденновск", "Бузулук", "Буйнакск", "Великие Луки", "Великий Новгород", "Верхняя Пышма", "Видное", "Владивосток", "Владикавказ", "Владимир", "Волгоград", "Волгодонск", "Волжск", "Волжский", "Вологда", "Вольск", "Воркута", "Воронеж", "Воскресенск", "Воткинск", "Всеволожск", "Выборг", "Выкса", "Вязьма", "Гатчина", "Геленджик", "Георгиевск", "Глазов", "Горно-Алтайск", "Грозный", "Губкин", "Гудермес", "Гуково", "Гусь-Хрустальный", "Дербент", "Дзержинск", "Димитровград", "Дмитров", "Долгопрудный", "Домодедово", "Донской", "Дубна", "Евпатория", "Егорьевск", "Ейск", "Екатеринбург", "Елабуга", "Елец", "Ессентуки", "Железногорск (Красноярский край)", "Железногорск (Курская область)", "Жигулевск", "Жуковский,Заречный", "Зеленогорск", "Зеленодольск", "Златоуст", "Иваново", "Ивантеевка", "Ижевск", "Избербаш", "Иркутск", "Искитим", "Ишим", "Ишимбай", "Йошкар-Ола", "Казань,Калининград", "Калуга", "Каменск-Уральский", "Каменск-Шахтинский", "Камышин", "Канск", "Каспийск", "Кемерово", "Керчь", "Кинешма", "Кириши", "Киров (Кировская область)", "Кирово-Чепецк", "Киселевск", "Кисловодск", "Клин", "Клинцы", "Ковров", "Когалым", "Коломна", "Комсомольск-на-Амуре", "Копейск", "Королев", "Кострома", "Котлас", "Красногорск", "Краснодар", "Краснокаменск", "Краснокамск", "Краснотурьинск", "Красноярск", "Кропоткин", "Крымск", "Кстово", "Кузнецк", "Кумертау", "Кунгур", "Курган", "Курск", "Кызыл", "Лабинск", "Лениногорск", "Ленинск-Кузнецкий", "Лесосибирск", "Липецк", "Лиски", "Лобня", "Лысьва", "Лыткарино", "Люберцы", "Магадан", "Магнитогорск", "Майкоп", "Махачкала", "Междуреченск", "Мелеуз", "Миасс", "Минеральные Воды", "Минусинск", "Михайловка", "Михайловск (Ставропольский край)", "Мичуринск", "Москва", "Мурманск", "Муром", "Мытищи", "Набережные Челны", "Назарово", "Назрань", "Нальчик", "Наро-Фоминск", "Находка", "Невинномысск", "Нерюнгри", "Нефтекамск", "Нефтеюганск", "Нижневартовск", "Нижнекамск", "Нижний Новгород", "Нижний Тагил", "Новоалтайск", "Новокузнецк", "Новокуйбышевск", "Новомосковск", "Новороссийск", "Новосибирск", "Новотроицк", "Новоуральск", "Новочебоксарск", "Новочеркасск", "Новошахтинск", "Новый Уренгой", "Ногинск", "Норильск", "Ноябрьск", "Нягань", "Обнинск", "Одинцово", "Озерск (Челябинская область)", "Октябрьский", "Омск", "Орел", "Оренбург", "Орехово-Зуево", "Орск", "Павлово,Павловский Посад", "Пенза", "Первоуральск", "Пермь", "Петрозаводск", "Петропавловск-Камчатский", "Подольск", "Полевской", "Прокопьевск", "Прохладный", "Псков", "Пушкино", "Пятигорск", "Раменское", "Ревда", "Реутов", "Ржев", "Рославль", "Россошь", "Ростов-на-Дону", "Рубцовск", "Рыбинск", "Рязань", "Салават", "Сальск", "Самара", "Санкт-Петербург", "Саранск", "Сарапул", "Саратов", "Саров", "Свободный", "Севастополь", "Северодвинск", "Северск", "Сергиев Посад", "Серов,Серпухов", "Сертолово", "Сибай", "Симферополь", "Славянск-на-Кубани", "Смоленск", "Соликамск", "Солнечногорск", "Сосновый Бор", "Сочи", "Ставрополь", "Старый Оскол", "Стерлитамак", "Ступино", "Сургут", "Сызрань", "Сыктывкар", "Таганрог", "Тамбов", "Тверь", "Тимашевск", "Тихвин", "Тихорецк", "Тобольск", "Тольятти", "Томск", "Троицк", "Туапсе", "Туймазы", "Тула", "Тюмень", "Узловая", "Улан-Удэ", "Ульяновск", "Урус-Мартан", "Усолье-Сибирское", "Уссурийск", "Усть-Илимск", "Уфа", "Ухта", "Феодосия", "Фрязино", "Хабаровск", "Ханты-Мансийск", "Хасавюрт", "Химки", "Чайковский", "Чапаевск", "Чебоксары", "Челябинск", "Черемхово", "Череповец", "Черкесск", "Черногорск", "Чехов", "Чистополь", "Чита", "Шадринск", "Шали", "Шахты", "Шуя", "Щекино", "Щелково", "Электросталь", "Элиста", "Энгельс", "Южно-Сахалинск", "Юрга", "Якутск", "Ялта", "Ярославль" };
+
+            foreach (var item in i)
+            {
+                Button button = new Button
+                {
+                    Content = item,
+                    FontSize = 18,
+                    FontFamily = new FontFamily("Merienda One"),
+                    Padding = new Thickness(10),
+                    Style = (Style)FindResource("ComboBoxButton")
+                };
+                ChangeCity.Children.Add(button);
+            }
+        }
+
+        private void Adventures_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressSecond.Content = ButtonPressNow.Content.ToString();
+            ButtonPressNow.Content = Adventures.Name.ToString();
+            Press++;
+            if (ButtonPressSecond.Content.ToString() == "BusTickets")
+            {
+                AnimationLeave(BusTickets);
+            }
+            if (ButtonPressSecond.Content.ToString() == "AirTickets")
+            {
+                AnimationLeave(AirTickets);
+            }
+            Adventures.Width = 140;
+        }
+
+        private void BusTickets_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressSecond.Content = ButtonPressNow.Content.ToString();
+            ButtonPressNow.Content = BusTickets.Name.ToString();
+            Press++;
+            if (ButtonPressSecond.Content.ToString() == "Adventures")
+            {
+                AnimationLeave(Adventures);
+            }
+            if (ButtonPressSecond.Content.ToString() == "AirTickets")
+            {
+                AnimationLeave(AirTickets);
+            }
+            BusTickets.Width = 140;
+        }
+
+        private void AirTickets_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressSecond.Content = ButtonPressNow.Content.ToString();
+            ButtonPressNow.Content = AirTickets.Name.ToString();
+            Press++;
+            if (ButtonPressSecond.Content.ToString() == "Adventures")
+            {
+                AnimationLeave(Adventures);
+            }
+            if (ButtonPressSecond.Content.ToString() == "BusTickets")
+            {
+                AnimationLeave(BusTickets);
+            }
+            AirTickets.Width = 140;
+        }
+    }
+}
