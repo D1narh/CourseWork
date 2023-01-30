5521cae665e51ec24a1000a1d90e94a460ef76a0
Гурьянов Денис <dunis.guryanov@mail.ru>
2023-01-31 00:26:00 +0400

    SettingNumPeople Window

--- MaimApp/Views/Treaty/SettingNumPeople.xaml.cs
+++ MaimApp/Views/Treaty/SettingNumPeople.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -22,6 +23,25 @@ namespace MaimApp.Views.Treaty
         public SettingNumPeople()
         {
             InitializeComponent();
+        }
+
+        private void People_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            if (NumPeople.Visibility == Visibility.Hidden)
+            {
+                NumPeople.Visibility = Visibility.Visible;
+                anim.To = 156;
+                anim.Duration = TimeSpan.FromSeconds(0.25);
+                NumPeople.BeginAnimation(HeightProperty, anim);
+            }
+            else
+            {
+                anim.To = 0;
+                anim.Duration = TimeSpan.FromSeconds(0.1);
+                NumPeople.BeginAnimation(HeightProperty, anim);
+                NumPeople.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
