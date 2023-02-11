aa4b9217e9742254aafbb405eb4b839bed0b8e83
Гурьянов Денис <dunis.guryanov@mail.ru>
2023-02-12 02:10:07 +0400

    Читабельный код = ЛЮБОВЬ! Readable code = LOVE!

--- MaimApp/Views/MainWindow/MainProduct.xaml.cs
+++ MaimApp/Views/MainWindow/MainProduct.xaml.cs
@@ -28,45 +28,29 @@ namespace MaimApp.Views
     {
         private readonly Formatter _formatter = new Formatter();
 
-        int Press = 0;
+        object senderNow;
+        object senderSecond;
+
         public MainProduct()
         {
             InitializeComponent();
 
             Sortierung();
             ChangeSitys();
-            AirTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
-            BusTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
             Hotels.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            BusTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            PersonalArea.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
             Adventures.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
         }
 
-        public static string GetUserCountryByIp()
+        private void Hotels_MouseEnter(object sender, MouseEventArgs e)
         {
-            IpInfo ipInfo = new IpInfo();
-            try
-            {
-                string info = new WebClient().DownloadString("http://ipinfo.io");
-                ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
-                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
-                ipInfo.Country = myRI1.EnglishName;
-            }
-            catch (Exception)
-            {
-                ipInfo.Country = null;
-            }
-
-            return ipInfo.Country;
+            AnimationEnter(Hotels);
         }
 
-        private void AirTickets_MouseEnter(object sender, MouseEventArgs e)
+        private void Hotels_MouseLeave(object sender, MouseEventArgs e)
         {
-            AnimationEnter(AirTickets);
-        }
-
-        private void AirTickets_MouseLeave(object sender, MouseEventArgs e)
-        {
-            AnimationLeave(AirTickets);
+            AnimationLeave(Hotels);
         }
 
         private void BusTickets_MouseEnter(object sender, MouseEventArgs e)
@@ -89,36 +73,14 @@ namespace MaimApp.Views
             AnimationLeave(Adventures);
         }
 
-        private void Hotels_MouseEnter(object sender, MouseEventArgs e)
+        private void PersonalArea_MouseEnter(object sender, MouseEventArgs e)
         {
-            AnimationEnter(Hotels);
+            AnimationEnter(PersonalArea);
         }
 
-        private void Hotels_MouseLeave(object sender, MouseEventArgs e)
+        private void PersonalArea_MouseLeave(object sender, MouseEventArgs e)
         {
-            AnimationLeave(Hotels);
-        }
-        public void AnimationEnter(Button name)
-        {
-            DoubleAnimation anim = new DoubleAnimation();
-            anim.To = 140;
-            anim.Duration = TimeSpan.FromSeconds(0.1);
-            name.BeginAnimation(WidthProperty, anim);
-        }
-        public void AnimationLeave(Button name)
-        {
-            if (ButtonPressNow.Content.ToString() == name.Name.ToString())
-            {
-                return;
-            }
-            else
-            {
-
-                DoubleAnimation anim = new DoubleAnimation();
-                anim.To = 130;
-                anim.Duration = TimeSpan.FromSeconds(0.1);
-                name.BeginAnimation(WidthProperty, anim);
-            }
+            AnimationLeave(PersonalArea);
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
@@ -129,12 +91,6 @@ namespace MaimApp.Views
             SearchText.Visibility = Visibility.Hidden;
 
             GC.Collect();
-        }
-        public async Task LoadProduct()
-        {
-            ViewProduct viewProduct = new ViewProduct();
-
-            list.ItemsSource = await Task.Run(() => viewProduct.FillCatalog());
         }
 
         private void Sort_Click(object sender, RoutedEventArgs e)
@@ -156,6 +112,97 @@ namespace MaimApp.Views
             }
         }
 
+        private void City_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            if (ChangeCityGrid.Visibility == Visibility.Hidden)
+            {
+                ChangeCityGrid.Visibility = Visibility.Visible;
+                anim.To = 145;
+                anim.Duration = TimeSpan.FromSeconds(0.25);
+                ChangeCityGrid.BeginAnimation(HeightProperty, anim);
+            }
+            else
+            {
+                anim.To = 0;
+                anim.Duration = TimeSpan.FromSeconds(0.1);
+                ChangeCityGrid.BeginAnimation(HeightProperty, anim);
+                ChangeCityGrid.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void PersonalArea_Click(object sender, RoutedEventArgs e)
+        {
+            Senderar(sender);
+        }
+
+        private void Adventures_Click(object sender, RoutedEventArgs e)
+        {
+            Senderar(sender);
+        }
+
+        private void BusTickets_Click(object sender, RoutedEventArgs e)
+        {
+            Senderar(sender);
+        }
+
+        private void Hotels_Click(object sender, RoutedEventArgs e)
+        {
+            Senderar(sender);
+        }
+
+
+                                                                            //Учтасток кода с логикой
+
+
+        //Метод для убирания лишней ширины
+        public void Senderar(object sender)
+        {
+            //Запись в глобальные переменные кнопок
+            senderSecond = senderNow;
+            senderNow = sender;
+
+            if (senderSecond != null)//Если жмякнули на кнопку, где не было ВЫДВИНУТО
+            {
+                AnimationLeave(senderSecond);
+            }
+        }
+
+        //Анимация выдвижения
+        public void AnimationEnter(Button name)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.To = 140;
+            anim.Duration = TimeSpan.FromSeconds(0.1);
+            name.BeginAnimation(WidthProperty, anim);
+        }
+
+        //Анимация сворачивания
+        public void AnimationLeave(object sender)
+        {
+            //Идет проверка на то, стоит ли выполнять сворачивание кнопки ?
+            if (senderNow == sender) //Если нажал два раза на одну и туже кнопку то сработает это <-
+            {
+                return;
+            }
+            else
+            {
+                DoubleAnimation anim = new DoubleAnimation();
+                anim.To = 130;
+                anim.Duration = TimeSpan.FromSeconds(0.1);
+                (sender as Button).BeginAnimation(WidthProperty, anim);
+            }
+        }
+
+        //Метод загрузки товаров
+        public async Task LoadProduct()
+        {
+            ViewProduct viewProduct = new ViewProduct();
+
+            list.ItemsSource = await Task.Run(() => viewProduct.FillCatalog());
+        }
+
+        //Метод для заполнения кнопками Грид ChangeSort
         public void Sortierung()
         {
             var i = new List<string> { "По скидке", "Лучшие отзывы", "Цена: Сначала дешевле", "Цена: Сначала дороже" };
@@ -174,6 +221,7 @@ namespace MaimApp.Views
             }
         }
 
+        //Метод для заполнения кнопками Грид ChangeSity
         public void ChangeSitys()
         {
             //var i = new List<string> { "Абакан", "Азов", "Александров", "Алексин", "Альметьевск", "Анапа", "Ангарск", "Анжеро-Судженск", "Апатиты", "Арзамас", "Армавир", "Арсеньев", "Артем", "Архангельск", "Асбест", "Астрахань", "Ачинск", "Балаково", "Балахна", "Балашиха", "Балашов", "Барнаул", " Батайск", "Белгород", "Белебей", "Белово", "Белогорск (Амурская область)", "Белорецк,Белореченск", "Бердск", "Березники", "Березовский (Свердловская область)", "Бийск", "Биробиджан", "Благовещенск (Амурская область)", "Бор", "Борисоглебск", "Боровичи", "Братск", "Брянск", "Бугульма", "Буденновск", "Бузулук", "Буйнакск", "Великие Луки", "Великий Новгород", "Верхняя Пышма", "Видное", "Владивосток", "Владикавказ", "Владимир", "Волгоград", "Волгодонск", "Волжск", "Волжский", "Вологда", "Вольск", "Воркута", "Воронеж", "Воскресенск", "Воткинск", "Всеволожск", "Выборг", "Выкса", "Вязьма", "Гатчина", "Геленджик", "Георгиевск", "Глазов", "Горно-Алтайск", "Грозный", "Губкин", "Гудермес", "Гуково", "Гусь-Хрустальный", "Дербент", "Дзержинск", "Димитровград", "Дмитров", "Долгопрудный", "Домодедово", "Донской", "Дубна", "Евпатория", "Егорьевск", "Ейск", "Екатеринбург", "Елабуга", "Елец", "Ессентуки", "Железногорск (Красноярский край)", "Железногорск (Курская область)", "Жигулевск", "Жуковский,Заречный", "Зеленогорск", "Зеленодольск", "Златоуст", "Иваново", "Ивантеевка", "Ижевск", "Избербаш", "Иркутск", "Искитим", "Ишим", "Ишимбай", "Йошкар-Ола", "Казань,Калининград", "Калуга", "Каменск-Уральский", "Каменск-Шахтинский", "Камышин", "Канск", "Каспийск", "Кемерово", "Керчь", "Кинешма", "Кириши", "Киров (Кировская область)", "Кирово-Чепецк", "Киселевск", "Кисловодск", "Клин", "Клинцы", "Ковров", "Когалым", "Коломна", "Комсомольск-на-Амуре", "Копейск", "Королев", "Кострома", "Котлас", "Красногорск", "Краснодар", "Краснокаменск", "Краснокамск", "Краснотурьинск", "Красноярск", "Кропоткин", "Крымск", "Кстово", "Кузнецк", "Кумертау", "Кунгур", "Курган", "Курск", "Кызыл", "Лабинск", "Лениногорск", "Ленинск-Кузнецкий", "Лесосибирск", "Липецк", "Лиски", "Лобня", "Лысьва", "Лыткарино", "Люберцы", "Магадан", "Магнитогорск", "Майкоп", "Махачкала", "Междуреченск", "Мелеуз", "Миасс", "Минеральные Воды", "Минусинск", "Михайловка", "Михайловск (Ставропольский край)", "Мичуринск", "Москва", "Мурманск", "Муром", "Мытищи", "Набережные Челны", "Назарово", "Назрань", "Нальчик", "Наро-Фоминск", "Находка", "Невинномысск", "Нерюнгри", "Нефтекамск", "Нефтеюганск", "Нижневартовск", "Нижнекамск", "Нижний Новгород", "Нижний Тагил", "Новоалтайск", "Новокузнецк", "Новокуйбышевск", "Новомосковск", "Новороссийск", "Новосибирск", "Новотроицк", "Новоуральск", "Новочебоксарск", "Новочеркасск", "Новошахтинск", "Новый Уренгой", "Ногинск", "Норильск", "Ноябрьск", "Нягань", "Обнинск", "Одинцово", "Озерск (Челябинская область)", "Октябрьский", "Омск", "Орел", "Оренбург", "Орехово-Зуево", "Орск", "Павлово,Павловский Посад", "Пенза", "Первоуральск", "Пермь", "Петрозаводск", "Петропавловск-Камчатский", "Подольск", "Полевской", "Прокопьевск", "Прохладный", "Псков", "Пушкино", "Пятигорск", "Раменское", "Ревда", "Реутов", "Ржев", "Рославль", "Россошь", "Ростов-на-Дону", "Рубцовск", "Рыбинск", "Рязань", "Салават", "Сальск", "Самара", "Санкт-Петербург", "Саранск", "Сарапул", "Саратов", "Саров", "Свободный", "Севастополь", "Северодвинск", "Северск", "Сергиев Посад", "Серов,Серпухов", "Сертолово", "Сибай", "Симферополь", "Славянск-на-Кубани", "Смоленск", "Соликамск", "Солнечногорск", "Сосновый Бор", "Сочи", "Ставрополь", "Старый Оскол", "Стерлитамак", "Ступино", "Сургут", "Сызрань", "Сыктывкар", "Таганрог", "Тамбов", "Тверь", "Тимашевск", "Тихвин", "Тихорецк", "Тобольск", "Тольятти", "Томск", "Троицк", "Туапсе", "Туймазы", "Тула", "Тюмень", "Узловая", "Улан-Удэ", "Ульяновск", "Урус-Мартан", "Усолье-Сибирское", "Уссурийск", "Усть-Илимск", "Уфа", "Ухта", "Феодосия", "Фрязино", "Хабаровск", "Ханты-Мансийск", "Хасавюрт", "Химки", "Чайковский", "Чапаевск", "Чебоксары", "Челябинск", "Черемхово", "Череповец", "Черкесск", "Черногорск", "Чехов", "Чистополь", "Чита", "Шадринск", "Шали", "Шахты", "Шуя", "Щекино", "Щелково", "Электросталь", "Элиста", "Энгельс", "Южно-Сахалинск", "Юрга", "Якутск", "Ялта", "Ярославль" };
@@ -194,71 +242,5 @@ namespace MaimApp.Views
             }
         }
 
-        private void Adventures_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonPressSecond.Content = ButtonPressNow.Content.ToString();
-            ButtonPressNow.Content = Adventures.Name.ToString();
-            Press++;
-            if (ButtonPressSecond.Content.ToString() == "BusTickets")
-            {
-                AnimationLeave(BusTickets);
-            }
-            if (ButtonPressSecond.Content.ToString() == "AirTickets")
-            {
-                AnimationLeave(AirTickets);
-            }
-            Adventures.Width = 140;
-        }
-
-        private void BusTickets_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonPressSecond.Content = ButtonPressNow.Content.ToString();
-            ButtonPressNow.Content = BusTickets.Name.ToString();
-            Press++;
-            if (ButtonPressSecond.Content.ToString() == "Adventures")
-            {
-                AnimationLeave(Adventures);
-            }
-            if (ButtonPressSecond.Content.ToString() == "AirTickets")
-            {
-                AnimationLeave(AirTickets);
-            }
-            BusTickets.Width = 140;
-        }
-
-        private void AirTickets_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonPressSecond.Content = ButtonPressNow.Content.ToString();
-            ButtonPressNow.Content = AirTickets.Name.ToString();
-            Press++;
-            if (ButtonPressSecond.Content.ToString() == "Adventures")
-            {
-                AnimationLeave(Adventures);
-            }
-            if (ButtonPressSecond.Content.ToString() == "BusTickets")
-            {
-                AnimationLeave(BusTickets);
-            }
-            AirTickets.Width = 140;
-        }
-
-        private void City_Click(object sender, RoutedEventArgs e)
-        {
-            DoubleAnimation anim = new DoubleAnimation();
-            if (ChangeCityGrid.Visibility == Visibility.Hidden)
-            {
-                ChangeCityGrid.Visibility = Visibility.Visible;
-                anim.To = 145;
-                anim.Duration = TimeSpan.FromSeconds(0.25);
-                ChangeCityGrid.BeginAnimation(HeightProperty, anim);
-            }
-            else
-            {
-                anim.To = 0;
-                anim.Duration = TimeSpan.FromSeconds(0.1);
-                ChangeCityGrid.BeginAnimation(HeightProperty, anim);
-                ChangeCityGrid.Visibility = Visibility.Hidden;
-            }
-        }
     }
 }
